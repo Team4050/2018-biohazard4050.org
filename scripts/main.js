7a35e60e4f6ca1d15a2e8aad95a055444a679386
Carson Seese <cds17257@gmail.com>
2018-01-05 16:22:16 -0500

    Tweaked image slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  $(document).ready(function() {
-    $("#lightSlider").lightSlider(); 
-  });
-
   $("#nav-placeholder").load("static-elements/nav.html");
   $("#footer-placeholder").load("static-elements/footer.html");
 
   //Init stuff
   $('ul.tabs').tabs('select_tab', 'tab_id');
-  $('.carousel.carousel-slider').carousel({fullWidth:true});
+});
+
+$("#home-slider").glide({
+  type: "carousel",
+  autoheight: "true"
 });
